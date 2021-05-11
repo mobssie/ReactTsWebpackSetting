84389def3ebe7302fd38558caf0f6b8b31bd59e1
mobssie <mobssie.jung@gmail.com>
2021-05-11 14:24:14 +0900

    init setting

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development', // production
+    devtool: 'eval', // hidden-source-map (production 모드에서)
+    resolve: {
+        extensions: ['.jsx', '.js', '.tsx', '.ts'],
+    },
+    entry: {
+        app: './index.tsx'
+    },
+    module:{
+        rules:[{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+
+        }]
+    },
+    plugins: [
+        
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+    }
+}
